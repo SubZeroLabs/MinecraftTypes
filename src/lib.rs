1eb0f355bb5af0102d8830956f3c06dc3db93bab
CoreyShupe <coreyshupe101@gmail.com>
2021-10-24 12:26:14 -0400

    Implement remaining minecraft protocol number types.

--- src/lib.rs
+++ src/lib.rs
@@ -10,9 +10,10 @@ pub trait Encodable<T> {
     fn encode(&self) -> Result<Vec<u8>>;
 }
 
-pub mod primitive {
-    macro_rules! prim_type {
-        ($name:ident = $primitive:ty, |$decode_iterator:ident| $decoder:expr, |$encode_self:ident| $encoder:expr) => {
+const UNEXPECTED_EOF: &str = "Unexpected EOF in decoder.";
+
+macro_rules! prim_type {
+        ($name:ident = $primitive:ty) => {
             pub struct $name($primitive);
 
             impl $name {
@@ -27,35 +28,252 @@ pub mod primitive {
                 }
             }
 
-            impl super::Decodable<$name> for $name {
-                fn decode(bytes: Vec<u8>) -> super::Result<($name, Vec<u8>)> {
-                    let mut $decode_iterator = bytes.into_iter();
-                    $decoder
+            impl std::ops::Deref for $name {
+                type Target = $primitive;
+                fn deref(&self) -> &Self::Target {
+                    &self.0
                 }
             }
+        };
+        ($name:ident = $primitive:ty, |$encode_self:ident| $encoder:expr) => {
+            prim_type!($name = $primitive);
 
             impl super::Encodable<$name> for $name {
                 fn encode($encode_self: &Self) -> super::Result<Vec<u8>> {
-                    $encoder
+                    Ok($encoder)
+                }
+            }
+        };
+        ($name:ident = $primitive:ty, |$decode_iterator:ident| $decoder:expr, |$encode_self:ident| $encoder:expr) => {
+            prim_type!($name = $primitive, |$encode_self| $encoder);
+
+            impl super::Decodable<$name> for $name {
+                fn decode(bytes: Vec<u8>) -> super::Result<($name, Vec<u8>)> {
+                    let $decode_iterator = bytes.into_iter();
+                    $decoder
+                }
+            }
+        };
+        ($name:ident = $primitive:ty, |$pre:tt $decode_iterator:ident| $decoder:expr, |$encode_self:ident| $encoder:expr) => {
+            prim_type!($name = $primitive, |$encode_self| $encoder);
+
+            impl super::Decodable<$name> for $name {
+                fn decode(bytes: Vec<u8>) -> super::Result<($name, Vec<u8>)> {
+                    let $pre $decode_iterator = bytes.into_iter();
+                    $decoder
                 }
             }
         };
     }
 
+pub mod primitive {
+    fn require_bytes(
+        iterator: impl Iterator<Item = u8>,
+        size: usize,
+    ) -> super::Result<(Vec<u8>, Vec<u8>)> {
+        let vec: Vec<u8> = iterator.collect();
+        if vec.len() < size {
+            Err(String::from(super::UNEXPECTED_EOF))
+        } else if vec.len() == size {
+            Ok((vec, vec![]))
+        } else {
+            Ok((Vec::from(&vec[0..size]), Vec::from(&vec[size..vec.len()])))
+        }
+    }
+
     prim_type!(
         McBoolean = bool,
-        |iterator| {
+        |mut iterator| {
             if let Some(next_byte) = iterator.next() {
                 let bool_res = match next_byte {
                     0x00u8 => Ok(false),
                     0x01u8 => Ok(true),
                     _ => Err(String::from("")),
                 }?;
-                Ok((McBoolean(bool_res), iterator.collect::<Vec<u8>>()))
+                Ok((McBoolean(bool_res), iterator.collect()))
             } else {
-                Err(String::from("Unexpected EOF in decoder."))
+                Err(String::from(super::UNEXPECTED_EOF))
             }
         },
-        |self| Ok(if self.0 { vec![0x01u8] } else { vec![0x00u8] })
+        |self| if self.0 { vec![0x01u8] } else { vec![0x00u8] }
     );
+
+    prim_type!(
+        McByte = i8,
+        |mut iterator| {
+            if let Some(next_byte) = iterator.next() {
+                Ok((McByte(next_byte as i8), iterator.collect()))
+            } else {
+                Err(String::from(super::UNEXPECTED_EOF))
+            }
+        },
+        |self| vec![self.0 as u8]
+    );
+
+    prim_type!(
+        McUnsignedByte = u8,
+        |mut iterator| {
+            if let Some(next_byte) = iterator.next() {
+                Ok((McUnsignedByte(next_byte), iterator.collect()))
+            } else {
+                Err(String::from(super::UNEXPECTED_EOF))
+            }
+        },
+        |self| vec![self.0]
+    );
+
+    prim_type!(
+        McShort = i16,
+        |iterator| {
+            let (bytes, remaining) = require_bytes(iterator, 2)?;
+            let be_bytes = [bytes[0], bytes[1]];
+            let res = i16::from_be_bytes(be_bytes);
+            Ok((McShort(res), remaining))
+        },
+        |self| Vec::from(self.0.to_be_bytes())
+    );
+
+    prim_type!(
+        McUnsignedShort = u16,
+        |iterator| {
+            let (bytes, remaining) = require_bytes(iterator, 2)?;
+            let be_bytes = [bytes[0], bytes[1]];
+            let res = u16::from_be_bytes(be_bytes);
+            Ok((McUnsignedShort(res), remaining))
+        },
+        |self| Vec::from(self.0.to_be_bytes())
+    );
+
+    prim_type!(
+        McInteger = i32,
+        |iterator| {
+            let (bytes, remaining) = require_bytes(iterator, 4)?;
+            let be_bytes = [bytes[0], bytes[1], bytes[2], bytes[3]];
+            let res = i32::from_be_bytes(be_bytes);
+            Ok((McInteger(res), remaining))
+        },
+        |self| Vec::from(self.0.to_be_bytes())
+    );
+
+    prim_type!(
+        McLong = i64,
+        |iterator| {
+            let (bytes, remaining) = require_bytes(iterator, 8)?;
+            let be_bytes = [
+                bytes[0], bytes[1], bytes[2], bytes[3], bytes[4], bytes[5], bytes[6], bytes[7],
+            ];
+            let res = i64::from_be_bytes(be_bytes);
+            Ok((McLong(res), remaining))
+        },
+        |self| Vec::from(self.0.to_be_bytes())
+    );
+
+    prim_type!(
+        McFloat = f32,
+        |iterator| {
+            let (bytes, remaining) = require_bytes(iterator, 4)?;
+            let be_bytes = [bytes[0], bytes[1], bytes[2], bytes[3]];
+            let res = f32::from_be_bytes(be_bytes);
+            Ok((McFloat(res), remaining))
+        },
+        |self| Vec::from(self.0.to_be_bytes())
+    );
+
+    prim_type!(
+        McDouble = f64,
+        |iterator| {
+            let (bytes, remaining) = require_bytes(iterator, 8)?;
+            let be_bytes = [
+                bytes[0], bytes[1], bytes[2], bytes[3], bytes[4], bytes[5], bytes[6], bytes[7],
+            ];
+            let res = f64::from_be_bytes(be_bytes);
+            Ok((McDouble(res), remaining))
+        },
+        |self| Vec::from(self.0.to_be_bytes())
+    );
+}
+
+prim_type!(VarInt = i32);
+prim_type!(VarLong = i64);
+
+impl Decodable<VarInt> for VarInt {
+    fn decode(bytes: Vec<u8>) -> Result<(VarInt, Vec<u8>)> {
+        let mut value = 0i32;
+        let mut bit_offset = 0u32;
+        let mut iter = bytes.into_iter();
+        loop {
+            if bit_offset == 35 {
+                return Err(String::from("VarInt was too big."));
+            }
+
+            if let Some(next_byte) = iter.next() {
+                value |= i32::from(next_byte & 0b01111111).overflowing_shl(bit_offset).0;
+                bit_offset += 7;
+
+                if next_byte & 0b10000000 == 0 {
+                    break;
+                }
+            } else {
+                return Err(String::from(UNEXPECTED_EOF));
+            }
+        };
+        Ok((VarInt(value), iter.collect()))
+    }
+}
+
+impl Encodable<VarInt> for VarInt {
+    fn encode(&self) -> Result<Vec<u8>> {
+        let mut vec = Vec::new();
+        let mut temp = self.0.clone() as u32;
+        loop {
+            if temp & 0xFFFFFF80 == 0 {
+                vec.push(temp as u8);
+                return Ok(vec);
+            }
+
+            vec.push((temp & 0x7F | 0x80) as u8);
+            temp = temp.overflowing_shr(7).0;
+        };
+    }
+}
+
+impl Decodable<VarLong> for VarLong {
+    fn decode(bytes: Vec<u8>) -> Result<(VarLong, Vec<u8>)> {
+        let mut value = 0i64;
+        let mut bit_offset = 0u32;
+        let mut iter = bytes.into_iter();
+        loop {
+            if bit_offset == 70 {
+                return Err(String::from("VarInt was too big."));
+            }
+
+            if let Some(next_byte) = iter.next() {
+                value |= i64::from(next_byte & 0b01111111).overflowing_shl(bit_offset).0;
+                bit_offset += 7;
+
+                if next_byte & 0b10000000 == 0 {
+                    break;
+                }
+            } else {
+                return Err(String::from(UNEXPECTED_EOF));
+            }
+        };
+        Ok((VarLong(value), iter.collect()))
+    }
+}
+
+impl Encodable<VarLong> for VarLong {
+    fn encode(&self) -> Result<Vec<u8>> {
+        let mut vec = Vec::new();
+        let mut temp = self.0.clone() as u64;
+        loop {
+            if temp & 0xFFFFFFFFFFFFFF80 == 0 {
+                vec.push(temp as u8);
+                return Ok(vec);
+            }
+
+            vec.push((temp & 0x7F | 0x80) as u8);
+            temp = temp.overflowing_shr(7).0;
+        };
+    }
 }
--- tests/data_types.rs
+++ tests/data_types.rs
@@ -26,3 +26,109 @@ fn mc_bool_decode_empty_arr_fails() {
         panic!("Invalid array proceeded as valid.")
     }
 }
+
+#[test]
+fn mc_byte_should_translate_byte() -> Result<()> {
+    let vec = vec![0x1u8];
+    let (parse, _) = primitive::McByte::decode(vec)?;
+    assert_eq!(0x1i8, *parse);
+    let encoded = parse.encode()?;
+    assert_eq!(vec![0x1u8], encoded);
+    Ok(())
+}
+
+#[test]
+fn mc_varint_encode_examples() -> Result<()> {
+    let (res1, _) = VarInt::decode(vec![0x00])?;
+    assert_eq!(0, *res1);
+    let (res2, _) = VarInt::decode(vec![0x01])?;
+    assert_eq!(1, *res2);
+    let (res3, _) = VarInt::decode(vec![0x02])?;
+    assert_eq!(2, *res3);
+    let (res4, _) = VarInt::decode(vec![0x7f])?;
+    assert_eq!(127, *res4);
+    let (res5, _) = VarInt::decode(vec![0x80, 0x01])?;
+    assert_eq!(128, *res5);
+    let (res6, _) = VarInt::decode(vec![0xff, 0x01])?;
+    assert_eq!(255, *res6);
+    let (res7, _) = VarInt::decode(vec![0xdd, 0xc7, 0x01])?;
+    assert_eq!(25565, *res7);
+    let (res8, _) = VarInt::decode(vec![0xff, 0xff, 0x7f])?;
+    assert_eq!(2097151, *res8);
+    let (res9, _) = VarInt::decode(vec![0xff, 0xff, 0xff, 0xff, 0x07])?;
+    assert_eq!(2147483647, *res9);
+    let (res10, _) = VarInt::decode(vec![0xff, 0xff, 0xff, 0xff, 0x0f])?;
+    assert_eq!(-1, *res10);
+    let (res11, _) = VarInt::decode(vec![0x80, 0x80, 0x80, 0x80, 0x08])?;
+    assert_eq!(-2147483648, *res11);
+
+    assert_eq!(vec![0x00], res1.encode()?);
+    assert_eq!(vec![0x01], res2.encode()?);
+    assert_eq!(vec![0x02], res3.encode()?);
+    assert_eq!(vec![0x7f], res4.encode()?);
+    assert_eq!(vec![0x80, 0x01], res5.encode()?);
+    assert_eq!(vec![0xff, 0x01], res6.encode()?);
+    assert_eq!(vec![0xdd, 0xc7, 0x01], res7.encode()?);
+    assert_eq!(vec![0xff, 0xff, 0x7f], res8.encode()?);
+    assert_eq!(vec![0xff, 0xff, 0xff, 0xff, 0x07], res9.encode()?);
+    assert_eq!(vec![0xff, 0xff, 0xff, 0xff, 0x0f], res10.encode()?);
+    assert_eq!(vec![0x80, 0x80, 0x80, 0x80, 0x08], res11.encode()?);
+    Ok(())
+}
+
+#[test]
+fn mc_varlong_encode_examples() -> Result<()> {
+    let (res1, _) = VarLong::decode(vec![0x00])?;
+    assert_eq!(0, *res1);
+    let (res2, _) = VarLong::decode(vec![0x01])?;
+    assert_eq!(1, *res2);
+    let (res3, _) = VarLong::decode(vec![0x02])?;
+    assert_eq!(2, *res3);
+    let (res4, _) = VarLong::decode(vec![0x7f])?;
+    assert_eq!(127, *res4);
+    let (res5, _) = VarLong::decode(vec![0x80, 0x01])?;
+    assert_eq!(128, *res5);
+    let (res6, _) = VarLong::decode(vec![0xff, 0x01])?;
+    assert_eq!(255, *res6);
+    let (res7, _) = VarLong::decode(vec![0xff, 0xff, 0xff, 0xff, 0x07])?;
+    assert_eq!(2147483647, *res7);
+    let (res8, _) = VarLong::decode(vec![0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x7f])?;
+    assert_eq!(9223372036854775807, *res8);
+    let (res9, _) = VarLong::decode(vec![
+        0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x01,
+    ])?;
+    assert_eq!(-1, *res9);
+    let (res10, _) = VarLong::decode(vec![
+        0x80, 0x80, 0x80, 0x80, 0xf8, 0xff, 0xff, 0xff, 0xff, 0x01,
+    ])?;
+    assert_eq!(-2147483648, *res10);
+    let (res11, _) = VarLong::decode(vec![
+        0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x01,
+    ])?;
+    assert_eq!(-9223372036854775808, *res11);
+
+    assert_eq!(vec![0x00], res1.encode()?);
+    assert_eq!(vec![0x01], res2.encode()?);
+    assert_eq!(vec![0x02], res3.encode()?);
+    assert_eq!(vec![0x7f], res4.encode()?);
+    assert_eq!(vec![0x80, 0x01], res5.encode()?);
+    assert_eq!(vec![0xff, 0x01], res6.encode()?);
+    assert_eq!(vec![0xff, 0xff, 0xff, 0xff, 0x07], res7.encode()?);
+    assert_eq!(
+        vec![0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x7f],
+        res8.encode()?
+    );
+    assert_eq!(
+        vec![0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x01],
+        res9.encode()?
+    );
+    assert_eq!(
+        vec![0x80, 0x80, 0x80, 0x80, 0xf8, 0xff, 0xff, 0xff, 0xff, 0x01],
+        res10.encode()?
+    );
+    assert_eq!(
+        vec![0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x80, 0x01],
+        res11.encode()?
+    );
+    Ok(())
+}
