366752cc6c174d4ad8b497458caf7b3e79cd56fd
CoreyShupe <coreyshupe101@gmail.com>
2021-10-28 15:23:52 -0400

    Fix simple auto def.

--- src/macros.rs
+++ src/macros.rs
@@ -198,7 +198,7 @@ macro_rules! auto_string {
 #[macro_export]
 macro_rules! simple_auto_enum {
     ($($enum_name:ident; $index_type:ty { $($byte_representation:literal => $option_name:ident,)* })*) => {
-        auto_enum!($($enum_name; $index_type { $($byte_representation => $option_name,)* })*);
+        simple_auto_enum!($($enum_name; $index_type { $($byte_representation => $option_name,)* })*);
     };
     ($($enum_name:ident; $index_type:ty { $($byte_representation:literal => $option_name:ident,)* })*) => {
         $(
