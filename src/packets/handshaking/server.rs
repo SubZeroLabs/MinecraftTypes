ad36eceaff04496abb50283337e2fa28890f1e16
CoreyShupe <coreyshupe101@gmail.com>
2021-10-29 02:29:12 -0400

    Fix handshaking type def.

--- src/packets/handshaking/server.rs
+++ src/packets/handshaking/server.rs
@@ -2,11 +2,18 @@ use crate::VarInt;
 
 auto_string!(HandshakeServerAddress, 255);
 
+simple_auto_enum! {
+    NextState; VarInt {
+        1 => Status,
+        2 => Login,
+    }
+}
+
 auto_struct! {
     Handshake {
         protocol_version: VarInt,
         server_address: HandshakeServerAddress,
-        server_port: u8,
-        next_state: VarInt,
+        server_port: u16,
+        next_state: NextState,
     }
 }
