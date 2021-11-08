10c87614705170218532d02b4e01f7093d80707b
CoreyShupe <coreyshupe101@gmail.com>
2021-11-07 21:49:34 -0500

    Add deref and display for string types.

--- src/strings.rs
+++ src/strings.rs
@@ -38,9 +38,23 @@ macro_rules! auto_string {
             }
         }
 
+        impl std::fmt::Display for $name {
+            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                write!(f, "{}", &self)
+            }
+        }
+
         impl From<&$name> for String {
             fn from(internal: &$name) -> Self {
                 String::from(&internal.0)
+            }
+        }
+
+        impl std::ops::Deref for $name {
+            type Target = str;
+
+            fn deref(&self) -> &Self::Target {
+                &$crate::strings::McString::string(self)
             }
         }
     };
